9e302c5c66fa5e775d2f0bd5b8057272f8769306
Chris <chrise.zhu@mail.utoronto.ca>
2017-07-22 18:50:59 -0400

    store simulator

--- ProjectPhase1/src/StoreSimulator.java
+++ ProjectPhase1/src/StoreSimulator.java
@@ -3,61 +3,36 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * A class that simulates a store.
  */
-
-
-
-// Events.txt commands
-//Commands: (based on methods. Can add to them later
-// Get information: [UPC]
-// Set
-// Add item: [*constructor arguments*]
-// Remove
-// Sell
-// Return
-
-
-
 public class StoreSimulator {
+
+  private static final Logger logger = Logger.getLogger(StoreSimulator.class.getName());
+  private static final Handler consoleHandler = new ConsoleHandler();
+
 
   public static void main(String[] args)
       throws IOException, FileNotFoundException, ClassNotFoundException {
+    
     BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Enter a file path: ");
     String fileName = kbd.readLine();
     Store s = new Store(fileName);
+    logger.info("a store has been created hoho");
 
-    System.out.println("What would you like to do? Type \'exit\' to exit.");
-    String input = kbd.readLine();
-    while (!(input.equals("exit"))) {
-      if (input.equals("UPC")) {
-        System.out.println("Please enter the UPC:");
-          String k = kbd.readLine();
-        while (!(k.equals("exit"))) {
-          System.out.println(s.UPCLookup(k));
-          System.out.println("Please enter the UPC:");
-          k = kbd.readLine();
-        }
-      } else if (input.equals("add")) {
-        System.out.println("Please enter the UPC and the quantity. For example, "
-            + "10000,2");
-        String k = kbd.readLine();
-        while (!(k.equals("exit"))){
-          String[] thingToAdd = k.split("\\,");
-          System.out.println(
-              s.addInventory(thingToAdd[0], Double.parseDouble(thingToAdd[1])));
-          System.out.println("Please enter the UPC and the quantity. For example, "
-              + "10000,2");
-          k = kbd.readLine();
-        }
-
-      }
-    }
   }
 }
+
+
+
 
 
 
