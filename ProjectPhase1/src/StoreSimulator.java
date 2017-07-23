bac8f741d7fd5f446792a7759a8296e63e11aa8a
Chris <chrise.zhu@mail.utoronto.ca>
2017-07-23 09:24:42 -0400

    Added docstrings.

--- ProjectPhase1/src/StoreSimulator.java
+++ ProjectPhase1/src/StoreSimulator.java
@@ -16,14 +16,16 @@ import java.util.logging.SimpleFormatter;
 public class StoreSimulator {
 
 
-  protected static final Logger logger = Logger.getLogger(Store.class.getName());
-  protected static final Handler consoleHandler = new ConsoleHandler();
+  private static final Logger logger = Logger.getLogger(Store.class.getName());
+  private static final Handler consoleHandler = new ConsoleHandler();
 
 
 
   public static void main(String[] args)
       throws IOException, FileNotFoundException, ClassNotFoundException {
 
+    // Initialize a FileHandler, a consoleHandler, and a logger to output
+    // to log.txt
     final FileHandler fileHandler = new FileHandler("log.txt");
     logger.setLevel(Level.ALL);
     consoleHandler.setLevel(Level.ALL);
@@ -31,18 +33,21 @@ public class StoreSimulator {
     SimpleFormatter formatter = new SimpleFormatter();
     fileHandler.setFormatter(formatter);
 
+    // Ask the user for a file path for StoreItems.txt so that a Store can be
+    // created with the file's items.
     BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Enter a file path: ");
     String fileName = kbd.readLine();
     Store s = new Store(fileName, logger);
     logger.info("A store has been created.");
 
+
+    // Phase 1: Ask the user for a file path for Events.txt and processes all the
+    // events in the file using the Store object s.
     BufferedReader kbd2 = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Enter the file path for events.txt.");
     String eventsPath = kbd2.readLine();
     s.processEvents(eventsPath);
-
-
 
     }
 
