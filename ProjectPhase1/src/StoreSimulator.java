bb1441b75a594bccaff8a6dac4869163cfb5e0da
Chris <chrise.zhu@mail.utoronto.ca>
2017-07-22 21:07:49 -0400

    optimized storeSimulator so that it can take in events.txt

--- ProjectPhase1/src/StoreSimulator.java
+++ ProjectPhase1/src/StoreSimulator.java
@@ -15,20 +15,38 @@ import java.util.logging.SimpleFormatter;
  */
 public class StoreSimulator {
 
-  private static final Logger logger = Logger.getLogger(StoreSimulator.class.getName());
-  private static final Handler consoleHandler = new ConsoleHandler();
+
+  protected static final Logger logger = Logger.getLogger(Store.class.getName());
+  protected static final Handler consoleHandler = new ConsoleHandler();
+
 
 
   public static void main(String[] args)
       throws IOException, FileNotFoundException, ClassNotFoundException {
-    
+
+    final FileHandler fileHandler = new FileHandler("log.txt");
+    logger.setLevel(Level.ALL);
+    consoleHandler.setLevel(Level.ALL);
+    logger.addHandler(fileHandler);
+    SimpleFormatter formatter = new SimpleFormatter();
+    fileHandler.setFormatter(formatter);
+
     BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Enter a file path: ");
     String fileName = kbd.readLine();
-    Store s = new Store(fileName);
-    logger.info("a store has been created hoho");
+    Store s = new Store(fileName, logger);
+    logger.info("A store has been created.");
 
-  }
+    BufferedReader kbd2 = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the file path for events.txt.");
+    String eventsPath = kbd2.readLine();
+    s.processEvents(eventsPath);
+
+
+
+    }
+
+
 }
 
 
